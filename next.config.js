268e7a68ddc6b8ce06226a952acb251fd3f539e0
Nigel Peirce <nigelpeirce@me.com>
2026-01-20 18:03:10 +0000

    Remove invalid experimental.turbo config - Next.js 15 uses Webpack by default

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Explicitly disable Turbopack - force Webpack
-  experimental: {
-    turbo: false,
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
